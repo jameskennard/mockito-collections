b5ff8271b909f73d35fd3d5e596a92a4526f864d
James Kennard <james@webamoeba.co.uk>
2012-08-05 13:08:07 +0100

    Tidy up MockitoInjectionDetailsFactory

--- core/src/main/java/uk/co/webamoeba/mockito/collections/MockitoInjectionDetailsFactory.java
+++ core/src/main/java/uk/co/webamoeba/mockito/collections/MockitoInjectionDetailsFactory.java
@@ -13,9 +13,15 @@ import org.mockito.internal.util.reflection.FieldReader;
  * Factory that creates {@link InjectionDetails} from an {@link Object} based on the Mockito annotations
  * {@link InjectMocks} and {@link Mock}. Fields with the {@link InjectMocks} annotation are considered injectees. Fields
  * with the {@link Mock} annotation are considered injectables.
+ * 
+ * @author James Kennard
  */
 public class MockitoInjectionDetailsFactory {
 
+    /**
+     * @param object
+     * @return {@link InjectionDetails} created based on the Mockito annotations {@link Mock} and {@link InjectMocks}.
+     */
     public InjectionDetails createInjectionDetails(Object object) {
 	Set<Object> injectees = getFieldValues(object, InjectMocks.class);
 	Set<Object> injectables = getFieldValues(object, Mock.class);
@@ -23,10 +29,17 @@ public class MockitoInjectionDetailsFactory {
     }
 
     private Set<Object> getFieldValues(Object object, Class<? extends Annotation> annotationClass) {
-	Set<Field> fields = getAnnotatedFields(object, annotationClass);
+	Set<Field> fields = getAnnotatedFields(object.getClass(), annotationClass);
 	return getFieldValues(object, fields);
     }
 
+    /**
+     * @param object
+     *            {@link Object} from which to retrieve values
+     * @param fields
+     *            The {@link Field Fields} from which to retrieve values
+     * @return Values of the {@link Field Fields} retrieved from the object
+     */
     private Set<Object> getFieldValues(Object object, Set<Field> fields) {
 	Set<Object> values = new HashSet<Object>();
 	for (Field field : fields) {
@@ -35,10 +48,20 @@ public class MockitoInjectionDetailsFactory {
 	return values;
     }
 
-    private Set<Field> getAnnotatedFields(Object object, Class<? extends Annotation> annotationClass) {
+    /**
+     * @param clazz
+     *            The {@link Class} from which we want to retrieve annotated {@link Field Fields}
+     * @param annotationClass
+     *            The annotation to use to identify the {@link Field Fields} we want to retrieve
+     * @return The {@link Field Fields} from the {@link Class} that are annotated with the {@link Annotation}
+     *         {@link Class}.
+     */
+    private Set<Field> getAnnotatedFields(Class<?> clazz, Class<? extends Annotation> annotationClass) {
 	Set<Field> mockDependentFields = new HashSet<Field>();
-	Class<?> clazz = object.getClass();
 	while (clazz != Object.class) {
+	    // We use getDeclaredFields because we want to get fields that are not otherwise visible. This means we also
+	    // want to look through the inheritance hierarchy because getDeclaredFields will not do
+	    // this for us
 	    Field[] fields = clazz.getDeclaredFields();
 	    for (Field field : fields) {
 		if (null != field.getAnnotation(annotationClass)) {
